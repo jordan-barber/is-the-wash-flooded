2004528278b437e777416e8013524a1302e7efb7
Jordan Barber <jdabarber5@gmail.com>
2024-10-15 20:57:57 +0000

    ADDED is it flooded

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Is The Wash Flooded?</h2>
+      <h1 className='blink'>Probably</h1>
+    </div>
+  );
+}
+
+export default App;
